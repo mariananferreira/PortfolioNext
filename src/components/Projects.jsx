b0367c8443e6c1b425f4a14d60101d71633ee8ab
Mariana Ferreira <62514533+mariananferreira@users.noreply.github.com>
2023-05-11 12:33:58 +0100

    change projects images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -142,7 +142,7 @@ export function Projects() {
                         {feature.description}
                       </p>
                     </div>
-                    <div className="mt-10 w-[45rem] overflow-hidden rounded-xl secondary shadow-xl shadow-purple-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
+                    <div className="mt-10 overflow-hidden rounded-xl secondary shadow-xl shadow-purple-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
                       <Image
                         className="w-full"
                         src={feature.image}

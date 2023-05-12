f5a32b02f961aeccba647ebceb94c99ece0b7f98
Mariana Ferreira <62514533+mariananferreira@users.noreply.github.com>
2023-05-12 19:36:38 +0100

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ export default function Home({ articles }) {
         />
       <Container className="mt-20">
         <div className="max-w-2xl">
-          <h1 className=" text-4xl font-bold tracking-tight firstTitle dark:text-zinc-100 sm:text-5xl">
+          <h1 className=" text-4xl font-bold tracking-tight firstTitle sm:text-5xl">
             Software Developer, <br/>System Architect, <br/>and Part-Time Traveler.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
